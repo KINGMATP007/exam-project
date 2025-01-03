10f0a02ae6130a20b965dde68a096cddfbcb7ccd
KINGMATP <kingmatp11@gmail.com>
2025-01-03 20:01:53 +0330

    Add files via upload

---
+++ code in action/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int[] MyArray = { 1, 3, 3, 3, 2, 5, 6, 7 };
+List<int> MyList = new List<int>();
+foreach (int i in MyArray)
+{
+    if (MyList.Contains(i) == false)
+    { MyList.Add(i); }
+}
+int[] SortedArray = MyList.ToArray();
---
+++ raw method/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int[] MyMethod(int[] input)
+{
+    List<int> MyList = new List<int>();
+    foreach (int i in input)
+    {
+        if (MyList.Contains(i) == false)
+        {
+            MyList.Add(i);
+        }
+
+    }
+    return MyList.ToArray();
+}
